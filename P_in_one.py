9416a3b4287dde992cff3030d409008955843d9c
Dhruv <vermadhruv572@gmail.com>
2024-02-26 10:29:10 +0530

    first commit

---
+++ P_in_one.py
@@ -0,0 +1,32 @@
+# helo helo helo
+
+# print(4.5, 'hello')
+# <---------------------------------->
+
+# print('hello','world',end='\n')
+# <---------------------------------->
+
+# hello = 'hello - 2'
+# world = 'bye'
+# print(hello, world)
+# <---------------------------------->
+
+# name = input('enter name : ')
+# print('your name is ',name)
+# <---------------------------------->
+
+# x = 9
+# y = 11
+# print(x/y) # returns quotient
+# print(x//y) # floor division
+# result =  int (x/y) # typescast float resultant to int
+# print(result)
+# print(x ** y) # raised to power
+# print(x%y) # returns remainder
+# print('sum of ',x,'and', y,'is', x+y )
+#print (x%y) * 2
+
+num = int(input("enter the number  :"))
+print(num - 5)
+# <---------------------------------->
+
